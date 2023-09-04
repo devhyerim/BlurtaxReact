c93caabee67ec7b5d0de2997f2fcf0360117561a
Juwon <jjw1578@gmail.com>
2023-09-04 18:14:45 +0900

    [ADD] rejecttable , [MOD] state

--- src/components/common/TAHeader.js
+++ src/components/common/TAHeader.js
@@ -1,12 +1,11 @@
-import '../../resources/assets/css/header.css';
-import '../../resources/assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { handleBodyClick } from '../../redux/sidebarSlice'; 
+import "../../resources/assets/css/header.css";
+import "../../resources/assets/vendor/bootstrap-icons/bootstrap-icons.css";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { handleBodyClick } from "../../redux/sidebarSlice";
 
 // 사이드바 메뉴 토글 함수를 받음
 const TAHeader = () => {
-
   // 프로필 토글
   const [profileIsOpen, setProfileIsOpen] = useState(false);
   // 메시지 토글
@@ -19,7 +18,7 @@ const TAHeader = () => {
 
   const msgToggle = () => {
     setMsgIsOpen(!msgIsOpen);
-  }
+  };
 
   return (
     <header id="header" className="header fixed-top d-flex align-items-center">
@@ -36,77 +35,176 @@ const TAHeader = () => {
       </div>
       <nav id="navbar" className="navbar">
         <ul>
-          <li className="dropdown"><a href="#"><span>수임처관리</span> <i className="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li className="dropdown">
+            <a href="#">
+              <span>수임처관리</span>{" "}
+              <i className="bi bi-chevron-down dropdown-indicator"></i>
+            </a>
             <ul className="dropnavs">
-              <li><a href="#">수임처 정보</a></li>
-              <li><a href="#">수임처별 업무체크리스트</a></li>
-              <li><a href="/docrequest/docrequestTA">민원서류 및 증명서류</a></li>
-              <li><a href="#">재무자료전송관리</a></li>
-              <li><a href="#">부가세신고현황리포트</a></li>
-              <li><a href="#">인건비신고현황리포트</a></li>
+              <li>
+                <a href="#">수임처 정보</a>
+              </li>
+              <li>
+                <a href="#">수임처별 업무체크리스트</a>
+              </li>
+              <li>
+                <a href="/docrequest/docrequestTA">민원서류 및 증명서류</a>
+              </li>
+              <li>
+                <a href="#">재무자료전송관리</a>
+              </li>
+              <li>
+                <a href="#">부가세신고현황리포트</a>
+              </li>
+              <li>
+                <a href="#">인건비신고현황리포트</a>
+              </li>
             </ul>
           </li>
-          <li className="dropdown"><a href="#"><span>기장업무</span><i className="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li className="dropdown">
+            <a href="#">
+              <span>기장업무</span>
+              <i className="bi bi-chevron-down dropdown-indicator"></i>
+            </a>
             <ul className="dropnavs">
-              <li><a href="#">기장진도현황</a></li>
-              <li><a href="#">자동전표처리</a></li>
-              <li><a href="/receipt/receiptta">증빙전표입력</a></li>
-              <li><a href="/bank/bankta">통장정리</a></li>
-              <li><a href="#">수임처 직원정보</a></li>
-              <li><a href="#">수임처 급여관리</a></li>
+              <li>
+                <a href="#">기장진도현황</a>
+              </li>
+              <li>
+                <a href="#">자동전표처리</a>
+              </li>
+              <li>
+                <a href="/receipt/receiptta">증빙전표입력</a>
+              </li>
+              <li>
+                <a href="/bank/bankta">통장정리</a>
+              </li>
+              <li>
+                <a href="#">수임처 직원정보</a>
+              </li>
+              <li>
+                <a href="#">수임처 급여관리</a>
+              </li>
             </ul>
           </li>
-          <li className="dropdown"><a href="#"><span>신고업무</span><i className="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li className="dropdown">
+            <a href="#">
+              <span>신고업무</span>
+              <i className="bi bi-chevron-down dropdown-indicator"></i>
+            </a>
             <ul className="dropnavs">
-              <li><a href="#">종합소득세 신고관리</a></li>
-              <li><a href="/info/infoTA">신고현황표</a></li>
-              <li><a href="#">신고상황분석표</a></li>
-              <li><a href="#">인사정보변동관리</a></li>
-              <li><a href="#">4대보험 일괄신고</a></li>
-              <li><a href="#">신용카드 매출자료조회</a></li>
-              <li><a href="#">고지 및 체납내역 조회</a></li>
-              <li><a href="#">사업용계좌신고 현황조회</a></li>
+              <li>
+                <a href="#">종합소득세 신고관리</a>
+              </li>
+              <li>
+                <a href="/info/infoTA">신고현황표</a>
+              </li>
+              <li>
+                <a href="#">신고상황분석표</a>
+              </li>
+              <li>
+                <a href="#">인사정보변동관리</a>
+              </li>
+              <li>
+                <a href="#">4대보험 일괄신고</a>
+              </li>
+              <li>
+                <a href="#">신용카드 매출자료조회</a>
+              </li>
+              <li>
+                <a href="#">고지 및 체납내역 조회</a>
+              </li>
+              <li>
+                <a href="#">사업용계좌신고 현황조회</a>
+              </li>
             </ul>
           </li>
-          <li className="dropdown"><a href="#"><span>청구/수금</span><i className="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li className="dropdown">
+            <a href="#">
+              <span>청구/수금</span>
+              <i className="bi bi-chevron-down dropdown-indicator"></i>
+            </a>
             <ul className="dropnavs">
-              <li><a href="#">청구서관리</a></li>
-              <li><a href="#">수금/정산관리</a></li>
-              <li><a href="#">출금동의관리</a></li>
+              <li>
+                <a href="#">청구서관리</a>
+              </li>
+              <li>
+                <a href="#">수금/정산관리</a>
+              </li>
+              <li>
+                <a href="#">출금동의관리</a>
+              </li>
             </ul>
           </li>
-          <li className="dropdown"><a href="#"><span>업무관리</span><i className="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li className="dropdown">
+            <a href="#">
+              <span>업무관리</span>
+              <i className="bi bi-chevron-down dropdown-indicator"></i>
+            </a>
             <ul className="dropnavs">
-              <li><a href="#">일일업무보고</a></li>
-              <li><a href="#">담당자별현황</a></li>
-              <li><a href="#">일일업무현황</a></li>
-              <li><a href="#">데이터내리기 히스토리</a></li>
-              <li><a href="#">수임기업 생성관리</a></li>
+              <li>
+                <a href="#">일일업무보고</a>
+              </li>
+              <li>
+                <a href="#">담당자별현황</a>
+              </li>
+              <li>
+                <a href="#">일일업무현황</a>
+              </li>
+              <li>
+                <a href="#">데이터내리기 히스토리</a>
+              </li>
+              <li>
+                <a href="#">수임기업 생성관리</a>
+              </li>
             </ul>
           </li>
-          <li className="dropdown"><a href="#"><span>설정관리</span><i className="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li className="dropdown">
+            <a href="#">
+              <span>설정관리</span>
+              <i className="bi bi-chevron-down dropdown-indicator"></i>
+            </a>
             <ul className="dropnavs">
-              <li><a href="#">공지사항</a></li>
-              <li><a href="#">권한관리</a></li>
-              <li><a href="#">수집정보등록</a></li>
-              <li><a href="#">홈택스인증서관리</a></li>
-              <li><a href="#">청구/수금설정</a></li>
+              <li>
+                <a href="#">공지사항</a>
+              </li>
+              <li>
+                <a href="#">권한관리</a>
+              </li>
+              <li>
+                <a href="#">수집정보등록</a>
+              </li>
+              <li>
+                <a href="#">홈택스인증서관리</a>
+              </li>
+              <li>
+                <a href="#">청구/수금설정</a>
+              </li>
             </ul>
           </li>
         </ul>
       </nav>
 
-
       <div className="search-bar">
-        <form className="search-form d-flex align-items-center" method="POST" action="#">
-          <input type="text" name="query" placeholder="Search" title="Enter search keyword" />
-          <button type="submit" title="Search"><i className="bi bi-search"></i></button>
+        <form
+          className="search-form d-flex align-items-center"
+          method="POST"
+          action="#"
+        >
+          <input
+            type="text"
+            name="query"
+            placeholder="Search"
+            title="Enter search keyword"
+          />
+          <button type="submit" title="Search">
+            <i className="bi bi-search"></i>
+          </button>
         </form>
       </div>
 
       <nav className="header-nav ms-auto">
         <ul className="d-flex align-items-center">
-
           <li className="nav-item d-block d-lg-none">
             <a className="nav-link nav-icon search-bar-toggle " href="#">
               <i className="bi bi-search"></i>
@@ -122,7 +220,11 @@ const TAHeader = () => {
             <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
               <li className="dropdown-header">
                 You have 4 new notifications
-                <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                <a href="#">
+                  <span className="badge rounded-pill bg-primary p-2 ms-2">
+                    View all
+                  </span>
+                </a>
               </li>
               <li>
                 <hr className="dropdown-divider" />
@@ -182,16 +284,10 @@ const TAHeader = () => {
               <li className="dropdown-footer">
                 <a href="#">Show all notifications</a>
               </li>
-
             </ul>
-
           </li>
 
-          <li
-            className="nav-item dropdown"
-            onClick={msgToggle}
-          >
-
+          <li className="nav-item dropdown" onClick={msgToggle}>
             <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
               <i className="bi bi-chat-left-text"></i>
               <span className="badge bg-success badge-number">3</span>
@@ -202,16 +298,20 @@ const TAHeader = () => {
                 className="dropdown-menu dropdown-menu-end 
                         dropdown-menu-arrow messages show"
                 style={{
-                  position: 'absolute',
-                  inset: '0px 0px auto auto',
-                  margin: '0px',
-                  transform: 'translate3d(-24.6667px, 35.3333px, 0px)',
+                  position: "absolute",
+                  inset: "0px 0px auto auto",
+                  margin: "0px",
+                  transform: "translate3d(-24.6667px, 35.3333px, 0px)",
                 }}
                 data-popper-placement="bottom-end"
               >
                 <li className="dropdown-header">
                   You have 3 new messages
-                  <a href="#"><span className="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                  <a href="#">
+                    <span className="badge rounded-pill bg-primary p-2 ms-2">
+                      View all
+                    </span>
+                  </a>
                 </li>
                 <li>
                   <hr className="dropdown-divider" />
@@ -219,10 +319,17 @@ const TAHeader = () => {
 
                 <li className="message-item">
                   <a href="#">
-                    <img src="/img/messages-1.jpg" alt="" className="rounded-circle" />
+                    <img
+                      src="/img/messages-1.jpg"
+                      alt=""
+                      className="rounded-circle"
+                    />
                     <div>
                       <h4>Maria Hudson</h4>
-                      <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                      <p>
+                        Velit asperiores et ducimus soluta repudiandae labore
+                        officia est ut...
+                      </p>
                       <p>4 hrs. ago</p>
                     </div>
                   </a>
@@ -233,10 +340,17 @@ const TAHeader = () => {
 
                 <li className="message-item">
                   <a href="#">
-                    <img src="/img/messages-2.jpg" alt="" className="rounded-circle" />
+                    <img
+                      src="/img/messages-2.jpg"
+                      alt=""
+                      className="rounded-circle"
+                    />
                     <div>
                       <h4>Anna Nelson</h4>
-                      <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                      <p>
+                        Velit asperiores et ducimus soluta repudiandae labore
+                        officia est ut...
+                      </p>
                       <p>6 hrs. ago</p>
                     </div>
                   </a>
@@ -247,10 +361,17 @@ const TAHeader = () => {
 
                 <li className="message-item">
                   <a href="#">
-                    <img src="/img/messages-3.jpg" alt="" className="rounded-circle" />
+                    <img
+                      src="/img/messages-3.jpg"
+                      alt=""
+                      className="rounded-circle"
+                    />
                     <div>
                       <h4>David Muldon</h4>
-                      <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                      <p>
+                        Velit asperiores et ducimus soluta repudiandae labore
+                        officia est ut...
+                      </p>
                       <p>8 hrs. ago</p>
                     </div>
                   </a>
@@ -262,28 +383,33 @@ const TAHeader = () => {
                 <li className="dropdown-footer">
                   <a href="#">Show all messages</a>
                 </li>
-
               </ul>
             )}
-
           </li>
 
-          <li
-            className="nav-item dropdown pe-3"
-            onClick={profileToggle}
-          >
-            <a className="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-              <img src="/img/taprofile.png" alt="Profile" className="rounded-circle" />
-              <span className="d-none d-md-block dropdown-toggle ps-2">박채원</span>
+          <li className="nav-item dropdown pe-3" onClick={profileToggle}>
+            <a
+              className="nav-link nav-profile d-flex align-items-center pe-0"
+              href="#"
+              data-bs-toggle="dropdown"
+            >
+              <img
+                src="/img/taprofile.png"
+                alt="Profile"
+                className="rounded-circle"
+              />
+              <span className="d-none d-md-block dropdown-toggle ps-2">
+                박채원
+              </span>
             </a>
             {profileIsOpen && (
               <ul
                 className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile show"
                 style={{
-                  position: 'absolute',
-                  inset: '0px 0px auto auto',
-                  margin: '0px',
-                  transform: 'translate3d(-16.6667px, 38px, 0px)',
+                  position: "absolute",
+                  inset: "0px 0px auto auto",
+                  margin: "0px",
+                  transform: "translate3d(-16.6667px, 38px, 0px)",
                 }}
                 data-popper-placement="bottom-end"
               >
@@ -296,7 +422,10 @@ const TAHeader = () => {
                 </li>
 
                 <li>
-                  <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                  <a
+                    className="dropdown-item d-flex align-items-center"
+                    href="users-profile.html"
+                  >
                     <i className="bi bi-person"></i>
                     <span>개인정보</span>
                   </a>
@@ -306,7 +435,10 @@ const TAHeader = () => {
                 </li>
 
                 <li>
-                  <a className="dropdown-item d-flex align-items-center" href="users-profile.html">
+                  <a
+                    className="dropdown-item d-flex align-items-center"
+                    href="users-profile.html"
+                  >
                     <i className="bi bi-gear"></i>
                     <span>환경설정</span>
                   </a>
@@ -316,7 +448,10 @@ const TAHeader = () => {
                 </li>
 
                 <li>
-                  <a className="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                  <a
+                    className="dropdown-item d-flex align-items-center"
+                    href="pages-faq.html"
+                  >
                     <i className="bi bi-question-circle"></i>
                     <span>도움말</span>
                   </a>
@@ -326,21 +461,21 @@ const TAHeader = () => {
                 </li>
 
                 <li>
-                  <a className="dropdown-item d-flex align-items-center" href="#">
+                  <a
+                    className="dropdown-item d-flex align-items-center"
+                    href="#"
+                  >
                     <i className="bi bi-box-arrow-right"></i>
                     <span>로그아웃</span>
                   </a>
                 </li>
-
               </ul>
             )}
           </li>
-
         </ul>
       </nav>
     </header>
   );
-}
-
+};
 
 export default TAHeader;
