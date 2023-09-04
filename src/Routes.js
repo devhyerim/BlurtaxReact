aca786ff67d3afb857857be7e5caa1c2593ea828
Juwon <jjw1578@gmail.com>
2023-09-04 09:27:59 +0900

    [add] route.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,35 +1,51 @@
-import BankCO from './pages/bank/BankCO';
-import BankTA from './pages/bank/BankTA';
-import MainTA from './pages/main/MainTA';
-import MainCO from './pages/main/MainCO';
-import LoginTA from './pages/main/LoginTA';
-import LoginCO from './pages/main/LoginCO';
+import BankCO from "./pages/bank/BankCO";
+import BankTA from "./pages/bank/BankTA";
+import MainTA from "./pages/main/MainTA";
+import MainCO from "./pages/main/MainCO";
+import LoginTA from "./pages/main/LoginTA";
+import LoginCO from "./pages/main/LoginCO";
 
 const Routes = [
   {
-    path: '/main/mainta',
-    component: MainTA
+    path: "/main/mainta",
+    component: MainTA,
   },
   {
-    path: '/main/mainco',
-    component: MainCO
+    path: "/main/mainco",
+    component: MainCO,
   },
   {
-    path: '/main/talogin',
-    component: LoginTA
+    path: "/main/talogin",
+    component: LoginTA,
   },
   {
-    path: '/main/cologin',
-    component: LoginCO
+    path: "/main/cologin",
+    component: LoginCO,
   },
   {
-      path: '/bank/bankco',
-      component: BankCO
+    path: "/bank/bankco",
+    component: BankCO,
   },
   {
-    path: '/bank/bankta',
-    component: BankTA
-  }
-]
+    path: "/bank/bankta",
+    component: BankTA,
+  },
+  {
+    path: "/register/registerta",
+    component: RegisterTA,
+  },
+  {
+    path: "/register/registerco",
+    component: RegisterTA,
+  },
+  {
+    path: "/receipt/receiptta",
+    component: ReceiptTA,
+  },
+  {
+    path: "/receipt/receiptco",
+    component: ReceiptCO,
+  },
+];
 
 export default Routes;
